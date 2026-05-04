bad3c01fd77a19a1b062a82060d4f822c88d0a0f
alex86trox-maker <alex86trox@gmail.com>
2026-05-04 14:09:45 -0500

    Update Message.js

--- models/models/Message.js
+++ models/models/Message.js
@@ -1,13 +1,34 @@
 const mongoose = require('mongoose');
 
 const messageSchema = new mongoose.Schema({
-  number: { type: String, required: true, index: true },
-  provider: { type: String, required: true },
-  from: { type: String, default: 'Unknown' },
-  text: { type: String, required: true },
-  otp: { type: String, default: null },
-  timestamp: { type: Date, default: Date.now }
-}, { timestamps: true });
+  number: {
+    type: String,
+    required: true,
+    index: true
+  },
+  provider: {
+    type: String,
+    required: true
+  },
+  from: {
+    type: String,
+    default: 'Unknown'
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  otp: {
+    type: String,
+    default: null
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now
+  }
+}, {
+  timestamps: true
+});
 
 messageSchema.index({ number: 1, timestamp: -1 });
 
